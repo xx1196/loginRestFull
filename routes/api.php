960efcb0c0f9e2dbc4026ce1e602e2b4a2937ad5
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-27 21:21:47 -0400

    Configurando login y traer usuarios por apirest

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('singup', 'AuthController@signup');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::post('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
