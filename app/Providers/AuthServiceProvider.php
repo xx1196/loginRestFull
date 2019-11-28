4cf954116becf78b52383ebb25e2ff7fec41d162
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-27 21:33:29 -0400

    Configurando expiracion de los tokens

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
@@ -29,6 +30,8 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
 
         Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addWeek());
 
         Passport::refreshTokensExpireIn(now()->addDays(30));
     }
