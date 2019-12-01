8119a56d1058d629d519fb82fa36230815271f9e
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-12-01 14:52:13 -0400

    Arreglando problemas de rutas

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,20 @@ Route::group(['middleware' => 'guest'], function () {
     Route::post('register', 'API\AuthController@register');
     Route::post('qr-generate', 'API\QrController@generate');
     Route::get('users/verify/{token}', 'API\UserController@verify')->name('verify');
+});
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', 'API\AuthController@logout');
 
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::post('logout', 'API\AuthController@logout');
+    Route::resource('users', 'API\UserController', [
+            'except' => ['create', 'edit']
+        ]
+    );
 
-        Route::resource('users', 'API\UserController', [
-                'except' => ['create', 'edit']
-            ]
-        );
+    Route::delete('users/deactivated/{user}', 'API\UserController@deactivated');
 
-        Route::delete('users/deactivated/{user}', 'API\UserController@deactivated');
+    Route::delete('users/activated/{user}', 'API\UserController@activated');
 
-        Route::delete('users/activated/{user}', 'API\UserController@activated');
+    Route::get('deactivatedUsers', 'API\UserController@deactivatedUsers');
 
-        Route::get('deactivatedUsers', 'API\UserController@deactivatedUsers');
-
-        Route::get('users/{user}/resend', 'API\UserController@resend')->name('resend');
-    });
+    Route::get('users/{user}/resend', 'API\UserController@resend')->name('resend');
 });
