df04097f289b784faa8ada72881b9d91ba51039c
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2020-01-25 23:27:03 -0500

    Agregando metodo para paginar resultados

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -76,4 +76,28 @@ trait ApiResponser
             200
         );
     }
+
+    private function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+
+        if (request()->has('per_page')) {
+            $perPage = (int)request()->per_page;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
 }
